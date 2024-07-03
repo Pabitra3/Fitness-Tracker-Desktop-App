d547e0809a6d8ad506b59acf920cc13e7f430f5a
Pabitra3 <pabitrasahoo3691@gmail.com>
2024-07-03 09:56:55 +0530

    Update file

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ class FitTrack(QWidget):
         super().__init__()
         self.settings()
         self.initUI()
+        self.button_click()
     
     # Settings
     def settings(self):
@@ -94,13 +95,62 @@ class FitTrack(QWidget):
         self.master_layout.addLayout(self.col1, 30)
         self.master_layout.addLayout(self.col2, 70)
         self.setLayout(self.master_layout)
+
+        self.load_table()
+    
+    # Events
+    def button_click(self):
+        self.add_btn.clicked.connect(self.add_workout)
+    
     
     # Load Tables
+    def load_table(self):
+        self.table.setRowCount(0)
+        query = QSqlQuery("SELECT * FROM fitness ORDER BY date DESC")
+        row = 0
+        while query.next():
+            fit_id = query.value(0)
+            date = query.value(1)
+            calories = query.value(2)
+            distance = query.value(3)
+            description = query.vlaue(4)
 
+            self.table.insertRow(row)
+            self.table.setItem(row, 0, QTableWidgetItem(str(fit_id)))
+            self.table.setItem(row, 1, QTableWidgetItem(date))
+            self.table.setItem(row, 2, QTableWidgetItem(str(calories)))
+            self.table.setItem(row, 3, QTableWidgetItem(str(distance)))
+            self.table.setItem(row, 4, QTableWidgetItem(description))
+            row += 1
+    
     # Add Tables
+    def add_workout(self):
+        date = self.date_box.date().toString("yyyy-MM-dd")
+        calories = self.kal_box.text()
+        distance = self.distance_box.text()
+        description = self.description.text()
+
+        query = QSqlQuery("""
+                          INSERT INTO fitness (date, calories, distance, description)
+                          VALUES (?,?,?,?)
+                          """)
+        query.addBindValue(date)
+        query.addBindValue(calories)
+        query.addBindValue(distance)
+        query.addBindValue(description)
+        query.exec_()
+
+        self.date_box.setDate(QDate.currentDate())
+        self.kal_box.clear()
+        self.distance_box.clear()
+        self.description.clear()
+
+        self.load_table()
+
 
     # Delete Tables
-
+    def delete_workout(self):
+        selected_row = self.table.currentRow()
     # Calculate Calories
 
     # Click
